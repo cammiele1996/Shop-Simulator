3d010d0c9e320ff34547dedb9ded7eb54b51f0d6
cammiele1996 <cammiele1996@gmail.com>
2026-06-04 16:30:37 -0400

    Set up money tracking and a blockout interior

---
+++ Assets/Scripts/StoreController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StoreController : MonoBehaviour
+{
+    public static StoreController instance;
+
+    public float currentMoney = 1000f;
+
+    public Transform stockSpawnPoint;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.iKey.wasPressedThisFrame)
+        {
+            AddMoney(100f);
+        }
+
+        if (Keyboard.current.oKey.wasPressedThisFrame)
+        {
+            if(CheckMoneyAvailable(250f))
+            {
+                SpendMoney(250f);
+            }
+        }
+
+    }
+
+    public void AddMoney(float amountToAdd)
+    {
+        currentMoney += amountToAdd;
+    }
+
+    public void SpendMoney(float amountToSpend)
+    {
+        currentMoney -= amountToSpend;
+
+        if(currentMoney < 0)
+        {
+            currentMoney = 0;
+        }
+    }
+
+    public bool CheckMoneyAvailable(float amountToCheck)
+    {
+        bool hasEnough = false;
+
+        if(currentMoney >= amountToCheck)
+        {
+            hasEnough = true;
+        }
+
+
+        return hasEnough;
+
+    }
+}
