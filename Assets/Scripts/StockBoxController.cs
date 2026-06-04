cf664d7413c22a85eafbfefcf872213ff2a58029
cammiele1996 <cammiele1996@gmail.com>
2026-06-04 02:53:55 -0400

    Setup stock box

---
+++ Assets/Scripts/StockBoxController.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockBoxController : MonoBehaviour
+{
+
+    public List<Transform> bigDrinkPoints, cerealPoints, tubeChipPoints, fruitPoints, largeFruitPoints;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
