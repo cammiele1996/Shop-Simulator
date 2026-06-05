81872358c5972b85f559809c91fc7ad35d7073fa
cammiele1996 <cammiele1996@gmail.com>
2026-06-05 15:43:30 -0400

    WIP: Adding stock to buy menu

---
+++ Assets/Scripts/BuyStockFrameController.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class BuyStockFrameController : MonoBehaviour
+{
+    public StockInfo info;
+
+    public TMP_Text nameText, priceText, amountInBoxText, boxPriceText, buttonText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
