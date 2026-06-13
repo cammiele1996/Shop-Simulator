4e69f41931d8de4b566289f585c9083be98b721c
cammiele1996 <cammiele1996@gmail.com>
2026-06-13 04:43:58 -0400

    Added stock box recycling to get rid of empty boxes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -69,6 +69,8 @@ public class PlayerController : MonoBehaviour
 
     public LayerMask whatIsStockBox;
 
+    public LayerMask whatIsBin;
+
     //                   ---Transforms---
 
     // References our hold point empty
@@ -353,12 +355,26 @@ public class PlayerController : MonoBehaviour
 
                 if (Mouse.current.leftButton.wasPressedThisFrame)
                 {
-                    if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf))
+                    if (heldBox.stockInBox.Count > 0)
                     {
-                        heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
 
-                        placeStockCounter = waitToPlaceStock;
+                        if (Physics.Raycast(ray, out hit, interactionRange, whatIsShelf))
+                        {
+                            heldBox.PlaceStockOnShelf(hit.collider.GetComponent<ShelfSpaceController>());
 
+                            placeStockCounter = waitToPlaceStock;
+
+                        }
+
+                    }
+                    else
+                    {
+                        if (Physics.Raycast(ray, out hit, interactionRange, whatIsBin))
+                        {
+                            Destroy(heldBox.gameObject);
+
+                            heldBox = null;
+                        }
                     }
                 }
 
