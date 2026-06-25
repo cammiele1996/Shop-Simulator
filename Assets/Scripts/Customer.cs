6b1e98f1f43d3ff47a1c79ad965575f323335bfc
cammiele1996 <cammiele1996@gmail.com>
2026-06-25 01:01:30 -0400

    Customer with walking/idle animations and points added

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+
+    public List<NavPoint> points = new List<NavPoint>();
+
+    public float moveSpeed;
+
+    public Animator anim;
+
+    private float currentWaitTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentWaitTime = points[0].waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (points.Count > 0)
+        {
+            MoveToPoint();
+        }
+        
+    }
+
+    public void MoveToPoint()
+    {
+
+        bool isMoving = true;
+
+        Vector3 targetPosition = new Vector3(points[0].point.position.x, transform.position.y, points[0].point.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        transform.LookAt(targetPosition);
+
+        if (Vector3.Distance(transform.position, targetPosition) < .25f)
+        {
+            isMoving = false;
+
+            currentWaitTime -= Time.deltaTime;
+
+            if (currentWaitTime <= 0)
+            {
+                StartNextPoint();
+            }
+        }
+
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    public void StartNextPoint()
+    {
+        if (points.Count > 0)
+        {
+            points.RemoveAt(0);
+
+            if (points.Count > 0)
+            {
+                currentWaitTime = points[0].waitTime;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class NavPoint
+{
+    public Transform point;
+    public float waitTime;
+}
